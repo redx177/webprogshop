8a2c3f54cac965c0eedb429df34c51644618f857
soultemptation <red_x@hotmail.com>
2014-06-03 16:13:48 +0200

    Fixing tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,6 @@
 class Order < ActiveRecord::Base
-  has_many :line_items, dependent: :destroy
-
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  has_many :line_items, dependent: :destroy
 
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
